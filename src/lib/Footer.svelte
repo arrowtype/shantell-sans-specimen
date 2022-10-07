3a7b72faa99765fcb6ba04ff8fee33826710cf69
arrowtype <arrowtypeco@gmail.com>
2022-10-07 16:23:27 -0400

    update footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,6 +1,6 @@
 <footer>
-    <p>Website made by <a target="_blank" href="https://arrowtype.com">Arrow Type</a>.</p>
     <p>“May you...” words by <a target="_blank" href="https://shantellmartin.art">Shantell Martin</a>.</p>
+    <p>Website by <a target="_blank" href="https://arrowtype.com">Arrow Type</a>.</p>
     <p class="line">------------</p>
     <p><a target="_blank" href="https://github.com/arrowtype/shantell-sans">Shantell Sans on GitHub</a></p>
     <p><a target="_blank" href="https://github.com/arrowtype/shantell-sans-specimen">View this website’s source</a></p>
