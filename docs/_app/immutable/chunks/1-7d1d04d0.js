c74a337c7139ab9d231cf18b97ece9c30a570bd4
arrowtype <arrowtypeco@gmail.com>
2022-10-28 11:44:04 -0400

    rebuild site

--- docs/_app/immutable/chunks/1-7d1d04d0.js
+++
@@ -1,1 +0,0 @@
-import{default as r}from"../components/error.svelte-f7fd38d8.js";import"./index-4b4199f8.js";import"./singletons-a3cc3a8f.js";export{r as component};
---
+++ docs/_app/immutable/chunks/1-9e8733ea.js
@@ -0,0 +1,1 @@
+import{default as r}from"../components/error.svelte-aa99996a.js";import"./index-4b4199f8.js";import"./singletons-9c4c384a.js";export{r as component};
