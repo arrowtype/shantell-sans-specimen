0f97a494886f254ea1e83770bf0252fd09c61099
arrowtype <arrowtypeco@gmail.com>
2022-11-29 16:26:39 -0500

    build site

---
+++ docs/_app/immutable/chunks/1-fbfe84fb.js
@@ -0,0 +1,1 @@
+import{default as r}from"../components/error.svelte-9573ecd9.js";import"./index-69e7cea0.js";import"./singletons-e0e5ba3a.js";export{r as component};
---
+++ docs/_app/immutable/chunks/2-46cea12e.js
@@ -0,0 +1,1 @@
+import{default as r}from"../components/pages/_page.svelte-23414907.js";import"./index-69e7cea0.js";import"./stores-152382f5.js";export{r as component};
