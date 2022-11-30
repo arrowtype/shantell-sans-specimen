40db44db83c5ff8498e9f113c74e7c97cb5c3384
arrowtype <arrowtypeco@gmail.com>
2022-11-30 15:42:17 -0500

    improve image caption font sizing for mobile

--- docs/_app/immutable/chunks/1-7d53acb7.js
+++
@@ -1,1 +0,0 @@
-import{default as r}from"../components/error.svelte-2cb91781.js";import"./index-69e7cea0.js";import"./singletons-38be56eb.js";export{r as component};
---
+++ docs/_app/immutable/chunks/1-b4f2605d.js
@@ -0,0 +1,1 @@
+import{default as r}from"../components/error.svelte-7c395eaf.js";import"./index-69e7cea0.js";import"./singletons-a98f4044.js";export{r as component};
