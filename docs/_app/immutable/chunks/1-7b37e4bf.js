7e1667ac5b78327878f227f5e1dfef905993bbf3
arrowtype <arrowtypeco@gmail.com>
2022-11-30 15:41:00 -0500

    improve image sizing for mobile

--- docs/_app/immutable/chunks/1-7b37e4bf.js
+++
@@ -1,1 +0,0 @@
-import{default as r}from"../components/error.svelte-0b56de1a.js";import"./index-69e7cea0.js";import"./singletons-0e6e0c27.js";export{r as component};
---
+++ docs/_app/immutable/chunks/1-7d53acb7.js
@@ -0,0 +1,1 @@
+import{default as r}from"../components/error.svelte-2cb91781.js";import"./index-69e7cea0.js";import"./singletons-38be56eb.js";export{r as component};
