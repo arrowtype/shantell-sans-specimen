fe216fe6ebedd246a55d0b769222a64d4179a042
arrowtype <arrowtypeco@gmail.com>
2022-12-21 12:39:37 -0700

    adjust font size of article h2

--- docs/_app/immutable/chunks/1-c1490e47.js
+++
@@ -1,1 +0,0 @@
-import{default as r}from"../components/error.svelte-1b529e5c.js";import"./index-69e7cea0.js";import"./singletons-6eb6531b.js";export{r as component};
---
+++ docs/_app/immutable/chunks/1-e06266df.js
@@ -0,0 +1,1 @@
+import{default as r}from"../components/error.svelte-8347fc63.js";import"./index-69e7cea0.js";import"./singletons-150dbca8.js";export{r as component};
