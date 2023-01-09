9b57fa6125f674c7fed3aae46e3e7c4ffcd9e79b
arrowtype <arrowtypeco@gmail.com>
2023-01-09 18:06:15 -0500

    build site

--- docs/_app/immutable/chunks/1-9780efe8.js
+++
@@ -1,1 +0,0 @@
-import{default as r}from"../components/error.svelte-17fae973.js";import"./index-69e7cea0.js";import"./singletons-0122a90c.js";export{r as component};
--- docs/_app/immutable/chunks/2-f017e468.js
+++
@@ -1,1 +0,0 @@
-import{default as r}from"../components/pages/_page.svelte-c15ec2ef.js";import"./index-69e7cea0.js";import"./stores-152382f5.js";export{r as component};
