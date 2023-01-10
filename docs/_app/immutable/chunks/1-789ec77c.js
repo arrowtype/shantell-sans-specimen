3d895221beb09d965222e196f2825bb42b41c56a
arrowtype <arrowtypeco@gmail.com>
2023-01-10 17:06:27 -0500

    improve opengraph metadata description

--- docs/_app/immutable/chunks/1-789ec77c.js
+++
@@ -1,1 +0,0 @@
-import{default as r}from"../components/error.svelte-0e2ac4d2.js";import"./index-69e7cea0.js";import"./singletons-28eff548.js";export{r as component};
---
+++ docs/_app/immutable/chunks/1-9c500f0f.js
@@ -0,0 +1,1 @@
+import{default as r}from"../components/error.svelte-02e7f53b.js";import"./index-69e7cea0.js";import"./singletons-f135ba77.js";export{r as component};
