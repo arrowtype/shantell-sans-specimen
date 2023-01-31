18c24e6d3b34bb0e31cf3269ccadd97ed5869382
arrowtype <arrowtypeco@gmail.com>
2023-01-31 10:47:31 -0500

    simplify universe 'in use' image

---
+++ docs/_app/immutable/chunks/1-7d195b32.js
@@ -0,0 +1,1 @@
+import{default as r}from"../components/error.svelte-fb02bac9.js";import"./index-69e7cea0.js";import"./singletons-b0a99d0a.js";export{r as component};
--- docs/_app/immutable/chunks/1-923b6128.js
+++
@@ -1,1 +0,0 @@
-import{default as r}from"../components/error.svelte-df9203fa.js";import"./index-69e7cea0.js";import"./singletons-7af2611d.js";export{r as component};
