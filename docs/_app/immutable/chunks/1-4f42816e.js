85b792da1e005ba30580b3d414c12ec996e700b1
arrowtype <arrowtypeco@gmail.com>
2023-02-13 17:05:15 -0500

    build

---
+++ docs/_app/immutable/chunks/1-4f42816e.js
@@ -0,0 +1,1 @@
+import{default as r}from"../components/error.svelte-7440f206.js";import"./index-572d2c78.js";import"./singletons-02026f9f.js";export{r as component};
--- docs/_app/immutable/chunks/2-35829744.js
+++
@@ -1,1 +0,0 @@
-import{default as r}from"../components/pages/_page.svelte-7f89afd3.js";import"./index-572d2c78.js";import"./stores-1fb25ca7.js";export{r as component};
