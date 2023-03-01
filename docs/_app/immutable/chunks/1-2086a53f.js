d204db3647568113188b138110bf6a80ea23c066
arrowtype <arrowtypeco@gmail.com>
2023-03-01 12:22:35 -0500

    tweak text accent and selection colors; build

--- docs/_app/immutable/chunks/1-2086a53f.js
+++
@@ -1,1 +0,0 @@
-import{default as r}from"../components/error.svelte-cb44ae20.js";import"./index-572d2c78.js";import"./singletons-5a3ad565.js";export{r as component};
---
+++ docs/_app/immutable/chunks/1-f50bc76b.js
@@ -0,0 +1,1 @@
+import{default as r}from"../components/error.svelte-4cd64369.js";import"./index-572d2c78.js";import"./singletons-3e8667ab.js";export{r as component};
