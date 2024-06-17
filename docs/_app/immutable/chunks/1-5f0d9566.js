9aac1c42d2b717e59facd0ac56a95b11a80ebedf
arrowtype <arrowtypeco@gmail.com>
2024-06-17 14:02:10 -0400

    update link to external blog post; rebuild site

---
+++ docs/_app/immutable/chunks/1-5f0d9566.js
@@ -0,0 +1,1 @@
+import{default as r}from"../components/error.svelte-0c1e24ec.js";import"./index-572d2c78.js";import"./singletons-2caa8066.js";export{r as component};
---
+++ docs/_app/immutable/chunks/3-504619c9.js
@@ -0,0 +1,1 @@
+import{default as r}from"../components/pages/process/_page.svelte-c092b5b0.js";import"./index-572d2c78.js";import"./stores-88b9f270.js";export{r as component};
