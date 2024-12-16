00d646bdfd2d486d8e2754c60d39541d0d5bf499
arrowtype <arrowtypeco@gmail.com>
2024-12-16 16:34:35 -0700

    build site with adjusted (R) mark

---
+++ docs/_app/immutable/chunks/1-b2ac37b5.js
@@ -0,0 +1,1 @@
+import{default as r}from"../components/error.svelte-af6cedfd.js";import"./index-572d2c78.js";import"./singletons-0bea89fd.js";export{r as component};
--- docs/_app/immutable/chunks/1-bf764eec.js
+++
@@ -1,1 +0,0 @@
-import{default as r}from"../components/error.svelte-bd5cd9a6.js";import"./index-572d2c78.js";import"./singletons-fac861eb.js";export{r as component};
